abf9b558e61efb5c2e7b4cf209a89b1d896d4b4a
ReinhardyOdhiambo <reinhardy.odhiambo@gmail.com>
2022-03-15 15:15:09 +0300

    responsive

--- src/components/Services.js
+++ src/components/Services.js
@@ -55,7 +55,10 @@ const Services = () => {
                        <img className='imagecard' src={shocks} alt=''/>
                        <div className='servicecard'>
                             <h4>Shock Repair</h4>
-                            <p>Details about shock repair </p>
+                            <p>Tailwind CSS works by scanning all of your HTML files, JavaScript components, 
+                                and any other templates for class names,
+                                 generating the corresponding styles and then writing them to a static CSS file.
+It's fast, flexible, and reliable — with zero-runtime. </p>
                         </div>
                   </div>
            
